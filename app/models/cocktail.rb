d2172b0ec4fc4bfbed2b43879fa45c73ee20cf31
Andrea dAlessandro <dalexandrea@gmail.com>
2017-11-30 12:22:34 +0100

    created models and controllers

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,5 @@
 class Cocktail < ApplicationRecord
+  has_many :ingredients through: :doses
+  has_many :doses
+  validates :name, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'doses/create'
+
+  get 'doses/new'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
 end
+
+
+  # root 'cocktails#index'
+
+  # resources :cocktails do
+  #   resources :doses
+  # end
+
+  # resources :ingredients do
+  #   resources :doses
+  # end
